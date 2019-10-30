2c563b10e8af41fb11e0d4d6cd0a83953faf858c
Borja <borja@propertista.com>
2019-10-30 18:52:56 +0100

    version patch

--- create-release.sh
+++ create-release.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+echo 'npm version patch'
 npm version patch
 
+echo 'fetching version'
 releaseBranch=v$(cat package.json | grep "\"version\"" | head -1 | awk -F: '{print $2}' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
+echo 'adding git files'
+echo 'old version' $releaseBranch
 git add .
+
+echo 'commit files'
 git commit -m "version patch"
 
+echo 'beforeTag' 
 git tag -a $releaseBranch -m 'release'
+
+echo 'beforePush'
 git push origin $releaseBranch
